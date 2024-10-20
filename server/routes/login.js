87225827a25d1b782ca0a9e7717d437148c1a4ec
Bidisha Das <officialbidisha1@gmail.com>
2024-10-20 16:08:21 +0530

    feat:added server changes

--- server/routes/login.js
+++ server/routes/login.js
@@ -10,14 +10,18 @@ router.post('/', async (req, res) => {
 
     try {
         const user = await User.findOne({ userId: { $regex: new RegExp(`^${userId}$`, 'i') } });
+        console.log('User', user);
         if (!user) {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
-
+        debugger;
+        const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: '1h' });
+        console.log('TOken', token);
         // Optionally, generate a JWT token here for user session
         // const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
         console.log('Login successful');
-        res.status(200).json({ message: 'Login successful' /*, token */ });
+        res.json({token});
+        // res.status(200).json({ message: 'Login successful' /*, token */ });
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal server error' });
